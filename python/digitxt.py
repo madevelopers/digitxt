ad2c1aed8ac861839a3a5e77ee2dd22cb8fb7699
Mark Anthony Degamo <madevelopers@gmail.com>
2013-04-24 09:43:16 +0800

    Started python implementation with completed test

---
+++ python/digitxt.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+hello = 's'
+
+
+class Digitxt(object):
+    """docstring for Digitxt"""
+    def __init__(self):
+        pass
+
+    def convert(self, num):
+        return 'hello'
---
+++ tests/python/test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import sys
+sys.path.append('../../python')
+
+import colorama
+from colorama import Fore
+
+import digitxt
+
+colorama.init(autoreset=True)
+_digitxt = digitxt.Digitxt()
+
+with open('../test-data.txt') as test_data_file:
+    for line in test_data_file:
+        data = line.split('=')
+        arg = data[0].strip()
+        expect = data[1].strip()
+        received = _digitxt.convert(arg)
+
+        if received == expect:
+            result = 'passed'
+            result_colored = '%s%s' % (Fore.GREEN, result)
+        else:
+            result = 'failed'
+            result_colored = '%s%s' % (Fore.RED, result)
+
+        print '%s: %s' % (arg, result_colored)
+        if result == 'failed':
+            print 'expected: %s%s' % (Fore.YELLOW, expect)
+            print 'received: %s%s' % (Fore.CYAN, received)
